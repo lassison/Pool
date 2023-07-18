98bd7aa03c459e8e7f9360527095dcf803a92b69
lassachraf <lassachraf1@gmail.com>
2023-07-18 22:13:02 +0100

    Ex07 done (C00)!

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+void ft_putchar (char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr (int nb)
+{
+    if (nb < 0)
+    {
+        ft_putchar('-');
+		nb = -nb;
+    }
+
+    if (nb >= 10)
+    {
+		ft_putchar(48 + nb / 10);
+		ft_putchar(48 + nb % 10);
+    }
+	else
+	{
+		ft_putchar('0' + nb);
+	}
+	
+}
+
+int main ()
+{
+    ft_putnbr(-6);
+    return 0;
+}
