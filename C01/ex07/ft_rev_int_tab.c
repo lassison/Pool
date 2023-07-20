a0857c45e12751224c196da9956dc67edd9cbd45
lassachraf <lassachraf1@gmail.com>
2023-07-21 00:07:37 +0100

    ex07 & ex08 are done, C01 is completely done !

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+void ft_rev_int_tab(int *tab, int size)
+{
+    int i = 0, swap;
+
+    while ( i < (size / 2) )
+    {
+        swap = tab[i];
+        tab[i] = tab[size - i - 1];
+        tab[size - i - 1] = swap;
+        i++;
+    }
+}
---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void ft_sort_int_tab(int *tab, int size)
+{
+	int sorted = 0;
+	while ( !sorted )
+	{
+		sorted = 1;
+		for(int i = 0; i < size; i++)
+		{
+			if(tab[i] > tab[i+1])
+			{
+				int temp;
+
+				temp = tab[i];
+				tab[i] = tab[i+1];
+				tab[i+1] = temp;
+				sorted = 0;
+			}
+		}
+	}
+}
